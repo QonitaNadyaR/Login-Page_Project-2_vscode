a16f410a8001ec21a40c2d252d48509f02cb0df9
Nitandy <qonitanadya14@gmail.com>
2023-11-30 16:12:41 +0700

    first commit

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,24 +1,35 @@
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/auth';
 import { ref } from 'vue';
+import axios from 'axios';
 import { useRouter } from 'vue-router';
+
 
 const auth = useAuthStore()
 const username = ref('')
 const password = ref('')
 const router = useRouter()
 
-const onLogin = () => {
-    const validUsername = 'Nitandy'
-    const validPassword = '54321'
-
-    if (username.value === validUsername && password.value === validPassword) {
-        auth.login(username.value)
-        router.push('/')
+const onLogin = async () => {
+    if (username.value === "" || password.value === "") {
+        alert("Username or Password can't be empty")
     } else {
-        window.alert('Username atau password salah')
+        try {
+            const response = await axios.post('http://localhost:3000/login', {
+                username: username.value,
+                password: password.value,
+            })
+
+            auth.login(username.value, password.value)
+            console.log(response);
+        } catch (error) {
+            console.error('Login error:', error);
+        }
     }
 }
+// auth.login(username.value)
+// router.push('/')
+
 </script>
 
 <template>
@@ -26,8 +37,8 @@ const onLogin = () => {
         <h1>Login Page</h1>
         <div class="flex flex-col gap-2">
             <input type="text" v-model="username" class="border p-2" placeholder="Username" />
-            <input type="password" v-model="password" class="border p-2" placeholder="Password" />
-            <button @click="onLogin" class="bg-blue-500 text-white py-1 px-3">Login</button>
+            <input type="text" v-model="password" class="border p-2" placeholder="Password" />
+            <button @click="onLogin()" class="bg-blue-500 text-white py-1 px-3">Login</button>
         </div>
     </div>
 </template>
